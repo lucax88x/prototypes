2850016322bfd7779711502108fc6548d1d59932
Firmani Emanuele <FIRE0272@adgr.net>
2016-12-17 15:04:32 +0100

    New OWIN api

---
+++ src/HelloWorld/IssuingService/Controllers/CardHolder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace IssuingService.Controllers
+{
+    public class CardHolderController : ApiController
+    {
+        [Route("api/cardholders")]
+        [HttpGet]
+        public List<string> GetCardHolders()
+        {
+            return new List<string> {"1", "2"};
+        }
+
+        [Route("api/cardholder/{id}")]
+        [HttpGet]
+        public string Get(string id)
+        {
+            return id + " ciao";
+        }
+    }
+}
---
+++ src/HelloWorld/IssuingService/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using Microsoft.Owin.Hosting;
+
+namespace IssuingService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseAddress = "http://localhost:9000/";
+
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                // Create HttpCient and make a request to api/values 
+                HttpClient client = new HttpClient();
+
+                var response = client.GetAsync(baseAddress + "api/cardholder/asdf").Result;
+                Console.WriteLine(response);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+
+                Console.WriteLine();
+                Console.WriteLine();
+
+                response = client.GetAsync(baseAddress + "api/cardholders").Result;
+                Console.WriteLine(response);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+
+                Thread.Sleep(Timeout.Infinite);
+            }
+        }
+    }
+}
