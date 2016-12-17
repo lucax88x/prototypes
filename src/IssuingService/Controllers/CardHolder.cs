5de1a55413d0b51a63ed428dd38fa689a9ce0def
Firmani Emanuele <FIRE0272@adgr.net>
2016-12-18 00:52:28 +0100

    Web returns an incremented counter stored in Redis

--- src/IssuingService/Controllers/CardHolder.cs
+++ src/IssuingService/Controllers/CardHolder.cs
@@ -1,15 +1,38 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Web.Http;
+using ServiceStack.Redis;
 
 namespace IssuingService.Controllers
 {
     public class CardHolderController : ApiController
     {
+
         [Route("api/cardholders")]
         [HttpGet]
         public List<string> GetCardHolders()
         {
-            return new List<string> {"1", "2"};
+            return new List<string> { "1", "2" };
+        }
+
+        [Route("api/cardholders/counter")]
+        [HttpGet]
+        public long GetCouter()
+        {
+            try
+            {
+                var redis = new RedisClient("redis", 6379);
+
+                var counter = redis.Increment("counter", 1);
+                return counter;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                return -1;
+            }
         }
 
         [Route("api/cardholder/{id}")]
