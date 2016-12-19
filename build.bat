80d62132942c82252abb155c9f153f0b62d1205d
Arialdo Martini <arialdomartini@gmail.com>
2016-12-19 19:11:03 +0100

    fixes #5 - build restores NuGet packages

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
+docker run -v $(pwd)/src:/src mono nuget restore /src/Issuing.sln
 docker run -v /work/prototypes/src:/src mono xbuild /src/Issuing.sln
 
 docker build -t adunicorn/issuing .
--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 set -e
 
+docker run -v $(pwd)/src:/src mono nuget restore /src/Issuing.sln
 docker run -v $(pwd)/src:/src mono xbuild /src/Issuing.sln
 
 docker build -t adunicorn/issuing .
