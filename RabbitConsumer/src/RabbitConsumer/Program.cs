2cc1d3f243d32cb837fb67bb1cd5bf4a43d0a06d
Firmani Emanuele <FIRE0272@adgr.net>
2016-12-20 01:17:00 +0100

    RabbitConsumer is registered to CardHolder#Add and stores input to Redis

--- RabbitConsumer/src/RabbitConsumer/Program.cs
+++ RabbitConsumer/src/RabbitConsumer/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using ServiceStack.Redis;
 
 namespace RabbitConsumer
 {
@@ -10,6 +12,67 @@ namespace RabbitConsumer
     {
         static void Main(string[] args)
         {
+            for (var i = 0; i < 10; i++)
+                try
+                {
+                    RegisterConsumer();
+                }
+                catch
+                {
+                    Thread.Sleep(5000);
+                }
         }
+
+        private static void RegisterConsumer()
+        {
+            var factory = new ConnectionFactory() { HostName = "rabbit" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "CardHolder", type: "topic", durable: true);
+
+                var queueName = channel.QueueDeclare("CardHolder.Add", true, false, false).QueueName;
+                channel.QueueBind(queue: queueName,
+                    exchange: "CardHolder",
+                    routingKey: "Add");
+
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    try
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] {0}", message);
+
+                        var cardHolder = JsonConvert.DeserializeObject<CardHolder>(message);
+                        var redis = new RedisClient("redis", 6379);
+                        redis.SetValue("cardholder_" + cardHolder.ID, message);
+                        Console.WriteLine("Stored in Redis: " + ("cardholder_" + cardHolder.ID));
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        Console.WriteLine(ex.StackTrace);
+                        throw;
+                    }
+                };
+
+                channel.BasicConsume(queue: queueName,
+                    noAck: true,
+                    consumer: consumer);
+
+                Console.WriteLine("RabbitConsumer registered");
+                Console.WriteLine();
+                Thread.Sleep(Timeout.Infinite);
+            }
+        }
+    }
+
+    public class CardHolder
+    {
+        public string ID { get; set; }
     }
 }
